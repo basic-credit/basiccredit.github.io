fabd2778476f29a75782778c2840f516c7bb3644
Jacob Dawson <jacobpauldawson@gmail.com>
2021-02-07 23:41:19 -0600

    Add funding page

---
+++ src/pages/funding.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const Funding = () => (
+  <Layout>
+    <SEO title="Funding" />
+    <h1>Funding</h1>
+  </Layout>
+);
+
+export default Funding;
