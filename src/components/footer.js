eadae5a4e9eda093bc5d425373de16227902f063
Jacob Dawson <jacobpauldawson@gmail.com>
2021-02-07 23:50:51 -0600

    wip: footer, not sticky yet

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,3 +1,6 @@
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import Logo from '../images/gatsby-icon.png';
+import { Link } from 'gatsby';
 import React from 'react';
 
 const Footer = () => {
@@ -7,9 +10,36 @@ const Footer = () => {
         marginTop: `2rem`,
       }}
     >
-      © {new Date().getFullYear()}, Built with
-      {` `}
-      <a href="https://www.gatsbyjs.com">Gatsby</a>
+      <Navbar sticky="bottom" bg="dark" variant="dark">
+        <Container fluid>
+          <Link to="/">
+            <Navbar.Brand>
+              <img
+                alt=""
+                src={Logo}
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+              />{' '}
+              Basic Credit
+            </Navbar.Brand>
+          </Link>
+          <Nav className="justify-content-space-evenly">
+            <Link to="/funding">
+              <Nav.Link as="span">Funding</Nav.Link>
+            </Link>
+            <Link to="https://www.google.com/">
+              <Nav.Link as="span">Discord</Nav.Link>
+            </Link>
+            <Link to="/about">
+              <Nav.Link as="span">About</Nav.Link>
+            </Link>
+            <Link to="/app">
+              <Nav.Link as="span">Use Basic Credit</Nav.Link>
+            </Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </footer>
   );
 };
