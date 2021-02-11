e2295d82112cf777572e1e7955d88faf7150e5aa
Jacob Dawson <jacobpauldawson@gmail.com>
2021-02-10 18:20:05 -0600

    Add gatsby-plugin-react-svg with test svg

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import CashSvg from '../assets/cash.inline.svg';
 
 const Faq = () => (
   <Layout>
     <SEO title="FAQ" />
     <h1>FAQ</h1>
+    <CashSvg />
   </Layout>
 );
 
