87e0aa0ef2e99a0948c09e27e2f1997778a25f22
Jacob Dawson <jacobpauldawson@gmail.com>
2021-02-13 21:38:59 -0600

    Mobile friendly body (#4)
    * Remove cash svg
    
    * Mobile friendly but not pretty

--- src/pages/documentation.js
+++ src/pages/documentation.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import CashSvg from '../assets/cash.inline.svg';
 
 const Documentation = () => (
   <Layout>
     <SEO title="Documentation" />
     <h1>Documentation</h1>
-    <CashSvg />
   </Layout>
 );
 
--- src/pages/faq.js
+++ src/pages/faq.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import CashSvg from '../assets/cash.inline.svg';
 
 const Faq = () => (
   <Layout>
     <SEO title="FAQ" />
     <h1>FAQ</h1>
-    <CashSvg />
   </Layout>
 );
 
