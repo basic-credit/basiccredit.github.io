0262af09272ea34a0ac87a81c650f2355aeec611
Jacob Dawson <jacobpauldawson@gmail.com>
2021-02-18 21:50:57 -0600

    Move navDialog to workWithUsDialog

--- src/components/dialogs/workWithUsDialog.js
+++ src/components/dialogs/workWithUsDialog.js
@@ -1,32 +1,95 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-
-import NavDialog from './navDialog';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { NavDropdown } from 'react-bootstrap';
 
 const WorkWithUsDialog = ({ isMobile }) => {
-  const [workWithUsText, setWorkWithUsText] = useState('');
+  const [state, setState] = useState({
+    email: '',
+    message: '',
+    isDialogOpen: false,
+  });
 
-  const onChangeWorkWithUsText = (event) => {
-    setWorkWithUsText(event.target.value);
+  const onChangeEmail = (event) => {
+    setState({ ...state, email: event.target.value });
   };
 
-  const onClickSendContactUsEmail = () => {
-    console.log('requester email', workWithUsText);
+  const onChangeMessage = (event) => {
+    setState({ ...state, message: event.target.value });
   };
+
+  const onClickSend = () => {
+    console.log('requester email', state);
+  };
+
+  const onClickOpen = () => {
+    setState({ ...state, isDialogOpen: true });
+  };
+
+  const onClickClose = () => {
+    setState({ ...state, isDialogOpen: false });
+  };
+
+  const item = isMobile ? (
+    <ListItem style={{ color: '#007bff' }}>
+      <ListItemText primary="Work with Us" onClick={onClickOpen} />
+    </ListItem>
+  ) : (
+    <NavDropdown.Item onClick={onClickOpen}>Work with Us</NavDropdown.Item>
+  );
 
   return (
-    <NavDialog
-      buttonName="Work with Us"
-      dialogTitle="Work with Us"
-      contentText="Please provide your email address and we will reach out to you shortly"
-      inputId="work-with-us-dialog"
-      inputLabel="Email Address"
-      inputType="email"
-      positiveButtonText="Send"
-      onClickPositiveButton={onClickSendContactUsEmail}
-      onChangeText={onChangeWorkWithUsText}
-      isMobile={isMobile}
-    />
+    <div>
+      {item}
+      <Dialog
+        open={state.isDialogOpen}
+        onClose={onClickClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">Work with Us</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Please provide your email address and we will reach out to you
+            shortly
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="work-with-us-email-input"
+            label="Email Address"
+            type="email"
+            onChange={onChangeEmail}
+            fullWidth
+          />
+          <TextField
+            margin="dense"
+            id="work-with-us-message-input"
+            label="Message"
+            type="text"
+            multiline
+            rowsMax={4}
+            onChange={onChangeMessage}
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClickClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={onClickSend} color="primary">
+            Send
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
   );
 };
 
